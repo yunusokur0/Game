8ad84e935f588af299486033a0d2a2b53cd273b7
YUNUS <114630722+yunusokur0@users.noreply.github.com>
2024-07-11 20:08:22 +0300

    Update SoundManager.cs

--- Platform-Drop/Assets/Scripts/Runtime/Managers/SoundManager.cs
+++ Platform-Drop/Assets/Scripts/Runtime/Managers/SoundManager.cs
@@ -20,21 +20,6 @@ public class SoundManager : MonoBehaviour
         selectedSource.clip = soundData.Clip;
         selectedSource.Play();
     }
-    //public void OnGetTriggerSound(SoundType type, bool isFirstSource)
-    //{
-    //    AudioSource selectedSource = isFirstSource ? triggerAudioSource : triggerAudioSource1;
-
-    //    // Sadece 'triggerAudioSource' için kontrol yapılıyor
-    //    if (isFirstSource && triggerAudioSource.isPlaying)
-    //    {
-    //        // 'triggerAudioSource' hala bir ses çalıyorsa, yeni tetiklemeyi yok say
-    //        return;
-    //    }
-
-    //    SoundData soundData = _cdSound.SoundData.Find(sd => sd.Type == type);
-    //    selectedSource.clip = soundData.Clip;
-    //    selectedSource.Play();
-    //}
     private void OnEnable() => SubscribeEvents();
     private void SubscribeEvents()
     {
